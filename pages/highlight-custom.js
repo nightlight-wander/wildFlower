7d0383384ce77275ed28f62bb36623899c6297e3
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2022-02-09 20:34:53 +0530

    feat:homeand docs page added

---
+++ pages/highlight-custom.js
@@ -0,0 +1,3 @@
+document.querySelectorAll("code").forEach(function(element) {
+    element.innerHTML = element.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+});
